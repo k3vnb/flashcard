1a1099c4b70e72ca607eea02b559606aabf12603
John Sawiris and Kevin Boyle <kevin@>
2017-10-17 13:41:49 -0700

    add/remove background color to box on click event

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $(this).next().slideToggle(1000);
     $(this).slideToggle(900);
+    $(this).parent().css('background-color', 'lightgreen');
   });
   $(".disappear").click(function() {
     $(this).prev().slideToggle(900);
     $(this).slideToggle(1000);
+    $(this).parent().css("background-color", "white");
   });
 });
